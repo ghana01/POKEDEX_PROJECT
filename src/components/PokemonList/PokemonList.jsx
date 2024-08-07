b42bd9cf70fe4a4b263383234dafbc809e99945f
ghana01 <ghanshyammishra58258@gmail.com>
2024-08-07 17:02:37 +0530

    added css to pokedex

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -49,9 +49,15 @@ function PokemonList(){
         <div className="Pokemon-list-wrapper">
                <div>Pokemon List</div> 
 
-                {(isLoading) ? 'Loading....' : 
-                PokemonList.map((p) => <Pokemon name={p.name} image={p.image}  key={p.id} />)
-                }
+               <div className="pokemon-wrapper">
+                    {(isLoading) ? 'Loading....' : 
+                     PokemonList.map((p) => <Pokemon name={p.name} image={p.image}  key={p.id} />)
+                      }   
+               </div>
+               <div className="controls button">
+                  <button>Prev</button>
+                  <button>Next</button>
+               </div>
 
         </div>
 
